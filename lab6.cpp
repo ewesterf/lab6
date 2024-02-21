83ca339e69007b6b8a5d67800bac63a2a845d76f
Ema Nichole Westerfeld <ewesterf@purdue.edu>
2024-02-21 13:02:40 -0500

    done

--- lab6.cpp
+++ lab6.cpp
@@ -2,10 +2,71 @@
 //
 
 #include <iostream>
+#include <SFML/Graphics.hpp>
+
+using namespace sf;
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    string background = "images1/backgrounds/winter.png";
+    string foreground = "images1/characters/yoda.png";
+
+    Texture backgroundTex;
+    if (!backgroundTex.loadFromFile(background))
+    {
+        cout << "Couldn't Load Image" << endl;
+        exit(1);
+    }
+
+    Texture foregroundTex;
+    if (!foregroundTex.loadFromFile(foreground))
+    {
+        cout << "Couldn't Load Image" << endl;
+        exit(1);
+    }
+
+    Image backgroundImage;
+    backgroundImage = backgroundTex.copyToImage();
+    Image foregroundImage;
+    foregroundImage = foregroundTex.copyToImage();
+
+    Color greenscreen = foregroundImage.getPixel(0, 0);
+    int r = greenscreen.r;
+    int g = greenscreen.g;
+    int b = greenscreen.b;
+    cout << r << ", " << g << ", " << b;
+
+
+    Vector2u sz = backgroundImage.getSize();
+    for (int y = 0; y < sz.y; y++)
+    {
+        for (int x = 0; x < sz.x; x++)
+        {
+            //these two loops will run the code inside for each pixel in the bg image
+            //you can access the current pixel at x,y, ex:
+            Color example = foregroundImage.getPixel(x, y);
+            //color objects store the individual channel values like example.r, example.g, and example.b
+            Color replacement = backgroundImage.getPixel(x, y);
+            Color greenscreen(32, 214, 23);
+
+            if (example == greenscreen)
+            {
+                foregroundImage.setPixel(x, y, replacement);
+            }
+        }
+    }
+
+    //by default, just show the foreground image
+    RenderWindow window(VideoMode(1024, 768), "Here's the output");
+    Sprite sprite1;
+    Texture tex1;
+    tex1.loadFromImage(foregroundImage);
+    sprite1.setTexture(tex1);
+    window.clear();
+    window.draw(sprite1);
+    window.display();
+    while (true);
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
